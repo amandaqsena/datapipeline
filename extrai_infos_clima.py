823fa2b884832fb7fa26ff413a74124d898c87de
Amanda Queiroz Sena <amandasena97@gmail.com>
2024-03-25 09:38:57 -0300

    variaveis para virtual crossing api

---
+++ extrai_infos_clima.py
@@ -0,0 +1,21 @@
+import os
+from os.path import join
+import pandas as pd
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# intervalo de datas
+data_inicio = datetime.today()
+data_fim = data_inicio + timedelta(days=7)
+
+# formatando as datas
+data_inicio = data_inicio.strftime('%Y-%m-%d')
+data_fim = data_fim.strftime('%Y-%m-%d')
+
+city = 'Boston'
+key = os.environ['VIRTUAL_CROSSING_KEY']
+
+URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+            f'{city}/{data_inicio}/{data_fim}?unitGroup=metric&include=days&key={key}&contentType=csv')
